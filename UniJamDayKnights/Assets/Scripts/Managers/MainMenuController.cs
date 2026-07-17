cb79f92ac5ea4b52b420674f81a7454f148b46f2
sbm5661 <sbm5661@gmail.com>
2026-07-16 19:56:02 -0600

    updating bgm managers in scenes(currently except lv1)

--- UniJamDayKnights/Assets/Scripts/Managers/MainMenuController.cs
+++ UniJamDayKnights/Assets/Scripts/Managers/MainMenuController.cs
@@ -9,7 +9,7 @@ public class MainMenuController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        AudioManager.Instance?.PlayMenuBGM();
     }
 
     // Update is called once per frame
